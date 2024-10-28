35818c3b50e00ec6eea2cb4c29871edcffaf3371
Dug <77532032+MustaficIvan@users.noreply.github.com>
2024-10-28 10:51:48 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,33 @@
 # Module
 import-Module 'Terminal-Icons'
-oh-my-posh init pwsh --config "C:\Users\Ivan\scoop\apps\oh-my-posh\current\themes\takuya.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\Ivan\scoop\apps\oh-my-posh\current\themes\ivan.omp.json" | Invoke-Expression
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-function Copy-Path { $pwd.Path | clip }
-function Get-ComPort { Get-CimInstance -ClassName Win32_PnPEntity | where {$_.Name -like "*(COM*)*"} | Sort Name | Format-Table Name }
-function Get-Info { Get-Content $PROFILE.CurrentUserCurrentHost }
-function Stop-Task { param([string]$p1) Stop-Process -Name $p1 }
-function Get-History { Get-Content (Get-PSReadlineOption).HistorySavePath }
+function Copy-Path
+{ $pwd.Path | clip 
+}
+function Get-ComPort
+{ Get-CimInstance -ClassName Win32_PnPEntity | where {$_.Name -like "*(COM*)*"} | Sort Name | Format-Table Name 
+}
+function Get-Info
+{ Get-Content $PROFILE.CurrentUserCurrentHost 
+}
+function Stop-Task
+{ param([string]$p1) Stop-Process -Name $p1 
+}
+function Get-AllChildren
+{
+  Get-ChildItem -hidden; Write-Host "`n ---------------------------HIDDEN----------------------------- `n";ls
+}
 
+# Use the -> btop command here and ther 
+# To use ps2exe it has to be in Windows Powershell
 # Alias
 Set-Alias -Name grep -Value Select-String
 Set-Alias -Name ll -Value ls
+Set-Alias -Name la -Value Get-AllChildren
 Set-Alias -Name stop -Value Stop-Task 
 Set-Alias -Name ci -value Copy-Item
 New-Alias -Name path -value Copy-Path
@@ -21,7 +35,6 @@ Set-Alias -Name extract -value Expand-Archive
 Set-Alias -Name com -value Get-ComPort
 Set-Alias -Name task -value Get-Process
 Set-Alias -Name info -value Get-Info
-Set-Alias -Name hist -value Get-History
 
 Set-Alias -Name py -value python
 Set-Alias -Name p -value python
